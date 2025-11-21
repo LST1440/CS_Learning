76fb923b019b2b1fd1f702ff9f890a1cee23c382
LST <jjj613602@gmail.com>
2025-11-21 23:16:39 +0800

    Update Main.java to interactive mode

--- Main.java
+++ Main.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub! 這是我的第一次測試。");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("請輸入你的暱稱");
+
+        String name=scanner.next();
+
+        System.out.println("-----------------------");
+        System.out.println("嗨，"+name+"!");
+        System.out.println("恭喜!你的Git和Java環境(包含輸入功能)全部設定成功!");
+
+        scanner.close();
     }
 }
